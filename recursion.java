cb5557b3da68888e1fb1ecc5265580b58f992e45
bavrahami00 <bavrahami00@stuy.edu>
2019-02-02 11:47:48 -0500

    Set up sqrt function

--- recursion.java
+++ recursion.java
@@ -1,3 +1,14 @@
 public class recursion {
-  
+  public static double sqrt(double n) {
+    if (n <= 0) {
+      return 0;
+    }
+    return guess(n,1);
+  }
+  public static double guess(double n, double g) {
+    if ((Math.abs(g*g-n) / n) < 0.000001) {
+      return g;
+    }
+    return guess(n,(n/g+g)/2);
+  }
 }
