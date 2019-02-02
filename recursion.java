f9c31cf78507b7e2dac3942b5132c0a955657725
bavrahami00 <bavrahami00@stuy.edu>
2019-02-02 11:49:35 -0500

    Set up fib function

--- recursion.java
+++ recursion.java
@@ -11,4 +11,13 @@ public class recursion {
     }
     return guess(n,(n/g+g)/2);
   }
+  public static int fib(int n) {
+    return fib(n,0,1,0);
+  }
+  public static int fib(int n, int one, int two, int time) {
+    if (time == n) {
+      return one;
+    }
+    return fib(n,two,one+two,time+1);
+  }
 }
